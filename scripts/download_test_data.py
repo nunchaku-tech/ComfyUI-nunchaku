19f1d6e9227269f34dfc035f5627238ff7ba0bb0
muyangli <lmxyy1999@foxmail.com>
2025-05-19 17:08:03 -0400

    revert the version

--- scripts/download_test_data.py
+++ scripts/download_test_data.py
@@ -6,7 +6,7 @@ import wget
 
 if __name__ == "__main__":
     os.makedirs("input", exist_ok=True)
-    filenames = ["logo.png", "robot.png", "strawberry.png", "mushroom_depth.webp"]
+    filenames = ["logo.png", "robot.png", "strawberry.png", "strawberry-mask.ng", "mushroom_depth.webp"]
     for filename in tqdm(filenames):
         if os.path.exists(os.path.join("input", filename)):
             print(f"File {filename} already exists, skipping download.")
