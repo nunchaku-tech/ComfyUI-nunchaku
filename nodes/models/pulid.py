d5e70e2564b8b09a1761d0d4e1d3894f5192cf21
zmwv823 <913720128@qq.com>
2025-06-07 11:39:33 +0800

    Update.

--- nodes/models/pulid.py
+++ nodes/models/pulid.py
@@ -3,10 +3,18 @@ from types import MethodType
 
 import numpy as np
 import torch
+import os
 
 from nunchaku.models.pulid.pulid_forward import pulid_forward
-from nunchaku.pipeline.pipeline_flux_pulid import PuLIDPipeline
+# from nunchaku.pipeline.pipeline_flux_pulid import PuLIDPipeline
+from .pulid_flux_pipeline import PuLIDPipeline
 
+import folder_paths
+
+if "ipadapter" not in folder_paths.folder_names_and_paths:
+    folder_paths.folder_names_and_paths["ipadapter"] = os.path.join(folder_paths.models_dir, "ipadapter")
+if "pulid" not in folder_paths.folder_names_and_paths:
+    folder_paths.folder_names_and_paths["pulid"] = os.path.join(folder_paths.models_dir, "pulid")
 
 class NunchakuPulidApply:
     def __init__(self):
@@ -53,6 +61,9 @@ class NunchakuPulidLoader:
         return {
             "required": {
                 "model": ("MODEL", {"tooltip": "The nunchaku model."}),
+                "ipadapter_name": (["Auto"] + folder_paths.get_filename_list("ipadapter") + folder_paths.get_filename_list("pulid"), {"default": "Auto"}),
+                "clip_vision_name": (["Auto"] + folder_paths.get_filename_list("clip_vision"), {"default": "Auto"}),
+                "version": (["v0.9.0", "v0.9.1"], {"default": "v0.9.0", "tooltip": ""})
             }
         }
 
@@ -64,14 +75,35 @@ class NunchakuPulidLoader:
     CATEGORY = "Nunchaku"
     TITLE = "Nunchaku Pulid Loader"
 
-    def load(self, model):
+    def load(self, model, ipadapter_name, clip_vision_name, version):
+        if os.path.exists(os.path.join(folder_paths.models_dir, "pulid_flux_v0.9.0.safetensors")): #Check possible existing 0.9.0 pulid model.
+            pulid_path = os.path.join(folder_paths.models_dir, "pulid_flux_v0.9.0.safetensors")
+        elif os.path.exists(os.path.join(folder_paths.models_dir, "pulid_flux_v0.9.1.safetensors")): #Check possible existing 0.9.1 pulid model.
+            pulid_path = os.path.join(folder_paths.models_dir, "pulid_flux_v0.9.1.safetensors")
+        else:
+            pulid_path = folder_paths.get_full_path("pulid", f"pulid_flux_{version}.safetensors")
+            if pulid_path == None:
+                pulid_path = folder_paths.get_full_path("ipadapter", f"pulid_flux_{version}.safetensors")
+                if pulid_path == None:
+                    pulid_path = folder_paths.get_full_path("ipadapter", ipadapter_name)
+                    if pulid_path == None:
+                        pulid_path = folder_paths.get_full_path("pulid", ipadapter_name)
+            
+        clip_vision_name = 'EVA02_CLIP_L_336_psz14_s6B.pt' if clip_vision_name == "Auto" else clip_vision_name
+        clip_vision_path = folder_paths.get_full_path("clip_vision", clip_vision_name) #Auto will return None
+        if clip_vision_path == None: # Check possible existing eva_clip to avoid duplicate download.
+            clip_vision_path = folder_paths.get_full_path("text_encoders", 'EVA02_CLIP_L_336_psz14_s6B.pt')
+            if clip_vision_path is None:
+                clip_vision_path = folder_paths.get_full_path("clip", 'EVA02_CLIP_L_336_psz14_s6B.pt')
+        
         pulid_model = PuLIDPipeline(
             dit=model.model.diffusion_model.model,
             device=self.pulid_device,
             weight_dtype=self.weight_dtype,
             onnx_provider=self.onnx_provider,
+            clip_vision_path=clip_vision_path
         )
-        pulid_model.load_pretrain(self.pretrained_model)
+        pulid_model.load_pretrain(pulid_path, version)
 
         return (
             model,
